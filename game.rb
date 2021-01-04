914c162787b86561296db158c7a95feb1ec682ee
emtupp <emilyc.tupper@gmail.com>
2021-01-04 21:30:45 +0000

    set up question in question file

--- game.rb
+++ game.rb
@@ -2,30 +2,34 @@ require './player.rb'
 require './question.rb'
 
 
-
 class Game
 
   def initialize
     @player1 = Player.new()
     @player2 = Player.new()
+    @question = Question.new()
   end
 
   def start
 
-    num1 = rand(1...20)
-    num2 = rand(1...20)
-    puts "What does #{num1} plus #{num2} equal?"
+    @question.question
 
-    print "> "
-    answer = $stdin.gets.chomp
+    # num1 = rand(1...20)
+    # num2 = rand(1...20)
+    # puts "What does #{num1} plus #{num2} equal?"
 
-    if answer == "#{num1 + num2}"
-      puts "YES! You are correct."
-    else
-      puts "Seriously? No!"
-      false
-      @player1.score_adjust
-    end
+    # print "> "
+    # answer = $stdin.gets.chomp
+
+    # if answer == "#{num1 + num2}"
+    #   puts "YES! You are correct."
+    # else
+    #   puts "Seriously? No!"
+    #   false
+    #   @player1.score_adjust
+    # end
+
+    @player1.score_adjust
 
     if @player1.score > 0
       puts "P1: #{@player1.score}/3 vs P2: #{@player2.score}/3"
--- question.rb
+++ question.rb
@@ -1,23 +1,19 @@
 class Question
 
-  # def initialize
-  #   @num1 = rand(1...20)
-  #   @num2 = rand(1...20)
-  # end
+  def question
+    num1 = rand(1...20)
+    num2 = rand(1...20)
+    puts "What does #{num1} plus #{num2} equal?"
 
-  # def question
-  #   puts "What does #{num1} plus #{num2} equal?"
+    print "> "
+    answer = $stdin.gets.chomp
 
-  #   print "> "
-  #   answer = $stdin.gets.chomp
+    if answer == "#{num1 + num2}"
+      puts "YES! You are correct."
+    else
+      puts "Seriously? No!"
+    end
 
-  #   if answer == "#{num1 + num2}"
-  #     puts "YES! You are correct."
-  #   else
-  #     puts "Seriously? No!"
-  #     @player1.score_adjust
-  #   end
-
-  # end
+  end
 
 end
